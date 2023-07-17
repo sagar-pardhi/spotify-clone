fc1d34554e4a178925683af32582eff11f3d5fd9
sagar-pardhi <sgrpardhi007@gmail.com>
2023-07-17 14:01:15 +0530

    completed basic layout

---
+++ components/box.tsx
@@ -0,0 +1,16 @@
+import { twMerge } from "tailwind-merge";
+
+interface IBoxProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+export const Box: React.FC<IBoxProps> = ({ children, className }) => {
+  return (
+    <div
+      className={twMerge(`bg-neutral-900 rounded-lg h-fit w-full`, className)}
+    >
+      {children}
+    </div>
+  );
+};
